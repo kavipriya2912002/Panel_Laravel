1c3182787112612e09c62321571b813c15ae1387
kavipriya2912002 <kavipriya2912002@gmail.com>
2025-01-09 18:00:35 +0530

    fetch bills added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -698,7 +698,7 @@
                 <!-- Added Overflow-x-auto for responsiveness -->
                 <div class="overflow-x-auto">
                     <table class="text-left w-full">
-                        <thead class="bg-black text-white">
+                        <thead class="bg-black max-h-96 text-white">
                             <tr class="w-full mb-4">
                                 <th class="p-4">Transaction Type</th>
                                 <th class="p-4">User ID</th>
