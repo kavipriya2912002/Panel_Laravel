3623c892181da18800b16459b6650bd50142a4bd
kavipriya2912002 <kavipriya2912002@gmail.com>
2025-01-09 17:53:12 +0530

    fetch bills added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -696,7 +696,7 @@
                 </h3>
             
                 <!-- Added Overflow-x-auto for responsiveness -->
-                <div>
+                <div class="overflow-x-auto">
                     <table class="text-left w-full overflow-x-auto">
                         <thead class="bg-black text-white">
                             <tr class="w-full mb-4">
@@ -709,7 +709,7 @@
                                 <th class="p-4">Download Report</th>
                             </tr>
                         </thead>
-                        <tbody class="bg-grey-light overflow-y-auto max-h-96 block" id="transaction-table-body">
+                        <tbody class="bg-grey-light overflow-x-auto overflow-y-auto max-h-96 block" id="transaction-table-body">
                             <!-- Transaction rows will be inserted here dynamically -->
                         </tbody>
                     </table>
