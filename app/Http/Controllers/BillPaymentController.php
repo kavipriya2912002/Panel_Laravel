a3c2ce771433ac2c5229128e2064ff8d6e866ebe
kavipriya2912002 <kavipriya2912002@gmail.com>
2025-01-21 15:59:18 +0530

    bbps changed

--- app/Http/Controllers/BillPaymentController.php
+++ app/Http/Controllers/BillPaymentController.php
@@ -207,10 +207,8 @@ class BillPaymentController extends Controller
     public function GetAllTransactions()
     {
         // Fetch all transactions from the database
-        $transactions = AllTransaction::where('status', 'failed')->get();  // This limits results to only 'failed' transactions
-
-
-        Log::info($transactions);
+        $transactions = AllTransaction::all();
+       
         // Return the transactions as a JSON response
         return response()->json($transactions);
     }
